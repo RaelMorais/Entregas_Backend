a2d55e482df0dfc5a292d003c8b1959951bb37ca
RaelMorais <israelsantanamorais@outlook.com>
2025-04-15 11:33:59 -0300

    adicionando o swagger

--- f1/app/urls.py
+++ f1/app/urls.py
@@ -1,8 +1,25 @@
 from django.urls import path
 from .views import * 
 from . import views
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API da Fórmula 1",
+        default_version='v1',
+        description="Documentação da API para dados da Fórmula 1"
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
    path("piloto/", views.GetPostPiloto.as_view()),
    path('carro/', views.GetPostCarro.as_view()),
+   path('piloto/<int:pk>', views.PilotoUpdateDelete.as_view()),
+   path('carro/<int:pk>', views.PilotoUpdateDelete.as_view()),
+   path('redoc/', view=schema_view.with_ui('redoc', cache_timeout=0)),
+   path('doc/', view=schema_view.with_ui('swagger', cache_timeout=0)),
 ]
--- f1/app/views.py
+++ f1/app/views.py
@@ -4,7 +4,44 @@ from .models import *
 from .serializers import * 
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import serializers
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
+
+class CarroPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+@swagger_auto_schema(
+    operation_description='Lista todos os pilotos',
+    responses={
+        200: PilotoSerializer(many=True),
+        400: 'Error',
+    },
+    manual_parameters=[
+        openapi.Parameter(
+            'nome', 
+            openapi.IN_QUERY,
+            description = 'Filtrar pelo nome do piloto', 
+            type=openapi.TYPE_STRING
+        )
+    ]
+        
+)
+def get(self, request, *args, **kwargs):
+    return super().get(request, *args, **kwargs)
+
+@swagger_auto_schema(
+    operation_description='Cria um novo piloto',
+    request_body=PilotoSerializer,
+    responses={
+        201: PilotoSerializer,
+        400: 'Erro'
+    }
+)
+def post(self, request, *args, **kwargs):
+    return super().post(request, *args, **kwargs)
 
 class PilotoPagination(PageNumberPagination):
     page_size = 5
@@ -22,15 +59,40 @@ class GetPostPiloto(ListCreateAPIView):
         if nome:
            queryset = queryset.filter(nome__icontains=nome)
         return queryset
+    
     def perform_create(self, serializer):
         # Garantir que 'classificacao' seja comparado com um valor numérico
         if serializer.validated_data['time'] != 'Red Bull' and serializer.validated_data['classificacao'] <= 5:
             raise serializers.ValidationError('Somente a red bull pode ganhar')
         serializer.save()
-        
+
+class PilotoUpdateDelete(RetrieveUpdateDestroyAPIView):
+    queryset = Piloto.objects.all()
+    serializer_class = PilotoSerializer
+    lookup_field = 'pk'
+
+    @swagger_auto_schema(
+        operation_description='Pega o piloto do id fornecido',
+        responses={
+            200: PilotoSerializer,
+            404: 'Not Found', 
+            400: 'Error'
+        }
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
 class GetPostCarro(ListCreateAPIView):
     queryset = Carro.objects.all()
     serializer_class = CarroSerializer
+    pagination_class = CarroPagination
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        nome = self.request.query_params.get('nome')
+        if nome:
+           queryset = queryset.filter(nome__icontains=nome)
+        return querys
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -45,7 +107,43 @@ class GetPostCarro(ListCreateAPIView):
             raise serializers.ValidationError('Somente a red bull pode ganhar')
         serializer.save()
 
+class CarroUpdateDelete(RetrieveUpdateDestroyAPIView):
+    queryset = Carro.objects.all()
+    serializer_class = CarroSerializer
+    lookup_field = 'pk'
 
+@swagger_auto_schema(
+    operation_description='Lista todos os pilotos',
+    responses={
+        200: CarroSerializer(many=True),
+        400: 'Error',
+    },
+    manual_parameters=[
+        openapi.Parameter(
+            'nome', 
+            openapi.IN_QUERY,
+            description = 'Filtrar pelo nome do piloto', 
+            type=openapi.TYPE_STRING
+        )
+    ]
+        
+)
+def get(self, request, *args, **kwargs):
+    return super().get(request, *args, **kwargs)
+
+@swagger_auto_schema(
+    operation_description='Cria um novo piloto',
+    request_body=CarroSerializer,
+    responses={
+        201: CarroSerializer,
+        400: 'Erro'
+    }
+)
+def post(self, request, *args, **kwargs):
+    return super().post(request, *args, **kwargs)
 
 # Create your views here.
 
+
+
+# Fazer swagger carro do get, post, delete, put de todos piloto e carro
